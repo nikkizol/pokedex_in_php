993a97d4503587290b3d15e04b66e4eb5ac356f3
MZ <nikita.zholkovskyi@gmail.com>
2020-10-01 15:13:32 +0200

    fixed some errors

--- index.php
+++ index.php
@@ -8,61 +8,61 @@ error_reporting(E_ALL);
 
 if (isset ($_GET["name"])) {
     $idName = $_GET["name"];
-    $pokemon = @file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $idName);
-    $data = json_decode($pokemon, true);
-    if ($data['name'] !== NULL) {
-        $name = $data['name'];
-        $img = $data['sprites']["front_default"];
-        $randArr = [];
-        $moves = [];
-        $maxMove = 4;
+    if (@file_get_contents("https://pokeapi.co/api/v2/pokemon/" . $idName) === FALSE) {
+        echo "Please enter a correct name or id!!!";
+    } else {//checking if input is a pokemon
+        $pokemon = @file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $idName);
+        $data = json_decode($pokemon, true);
+        if (!empty ($data['name'])) {
+            $name = $data['name'];
+            $id = $data['id'];
+            $img = $data['sprites']["front_default"];
+            $randArr = [];
+            $moves = [];
+            $maxMove = 4;
 
 
-        for ($x = 0; $x < 4; $x++) {
-            array_push($randArr, rand(0, count($data['moves'])));
-        }
+            for ($x = 0; $x < 4; $x++) {
+                array_push($randArr, rand(0, count($data['moves'])));
+            }
 //echo var_dump($randArr);
-        for ($x = 0; $x < 4; $x++) {
-            array_push($moves, $data['moves'][$randArr[$x]]["move"]["name"]);
-        }
-        $uniqueMoves = array_unique($moves);
+            for ($x = 0; $x < 4; $x++) {
+                array_push($moves, $data['moves'][$randArr[$x]]["move"]["name"]);
+            }
+            $uniqueMoves = array_unique($moves);
 
 //echo var_dump($moves);
-        $pokemonSpices = @file_get_contents('https://pokeapi.co/api/v2/pokemon-species/' . $idName);
-        $dataSpices = json_decode($pokemonSpices, true);
+            $pokemonSpices = @file_get_contents('https://pokeapi.co/api/v2/pokemon-species/' . $idName);
+            $dataSpices = json_decode($pokemonSpices, true);
 
 //echo var_dump($dataSpices);
-        if ($dataSpices["evolves_from_species"] !== NULL) {
-            $pokemonSpicesFrom = $dataSpices["evolves_from_species"]["url"];
-            $dataSpicesFrom = json_decode($pokemonSpicesFrom, true);
+            if ($dataSpices["evolves_from_species"] !== NULL) {
+                $pokemonSpicesFrom = $dataSpices["evolves_from_species"]["url"];
+                $dataSpicesFrom = json_decode($pokemonSpicesFrom, true);
 
 //echo var_dump($pokemonSpicesFrom);
-            $dataPrevEvo = @file_get_contents($pokemonSpicesFrom);
-            $prevEvo = json_decode($dataPrevEvo, true);
-            $prevImg = @file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $prevEvo["name"]);
-            $data2 = json_decode($prevImg, true);
+                $dataPrevEvo = @file_get_contents($pokemonSpicesFrom);
+                $prevEvo = json_decode($dataPrevEvo, true);
+                $prevImg = @file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $prevEvo["name"]);
+                $data2 = json_decode($prevImg, true);
 
-            if ($data2['sprites'] !== NULL) {
-                $imgPrev = $data2['sprites']["front_default"];
-                $idPrev = $data2['id'];
+                if ($data2['sprites'] !== NULL) {
+                    $imgPrev = $data2['sprites']["front_default"];
+                    $idPrev = $data2['id'];
+                    $namePrev = $prevEvo["name"];
 
-            } else {
-                $imgPrev = "";
-                $idPrev = "";
+                }
+            } else if ($pokemonSpicesTo = $dataSpices["evolution_chain"]["url"] !== NULL) {
+                $pokemonSpicesTo = $dataSpices["evolution_chain"]["url"];
+                $dataSpicesTo = json_decode($pokemonSpicesTo, true);
+                $dataNextEvo = @file_get_contents($pokemonSpicesTo);
+                $nextEvo = json_decode($dataNextEvo, true);
+                $nameNextEvo = $nextEvo['chain']['evolves_to'][0]['species']['name'];
+                $data3 = @file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $nameNextEvo);
+                $dataNext = json_decode($data3, true);
+                $imgForNext = $dataNext['sprites']["front_default"];
 
             }
-            $namePrev = $prevEvo["name"];
-
-        } else {
-
-//            $dataSpices["evolves_from_species"] = " ";
-            $pokemonSpicesTo = $dataSpices["evolution_chain"]["url"];
-            $dataSpicesTo = json_decode($pokemonSpicesTo, true);
-            $dataNextEvo = @file_get_contents($pokemonSpicesTo);
-            $nextEvo = json_decode($dataNextEvo, true);
-//            echo var_dump($nextEvo['chain']['evolves_to'][0]['species']['name']);
-            $nameNextEvo = $nextEvo['chain']['evolves_to'][0]['species']['name'];
-
         }
     }
 }
@@ -77,13 +77,13 @@ if (isset ($_GET["name"])) {
 <ol class="Pokemon">
     <h4 class="title">
         <p class="name"><?php if (isset ($name)) {
-                echo ucfirst($name);
+                echo "Name: ", ucfirst($name);
             } else {
 
             } ?></p>
 
         <p class="id"><?php if (isset ($id)) {
-                echo $id;
+                echo "Id#: ", $id;
             } else {
             } ?></p>
     </h4>
@@ -94,11 +94,11 @@ if (isset ($_GET["name"])) {
     } ?>">
     <p class="moves"></p>
     <p id="moves"><?php if (isset ($uniqueMoves)) {
-            echo "moves: ", implode(" ", $uniqueMoves);
+            echo "Moves: ", implode(" ", $uniqueMoves);
         } else {
         } ?></p>
     <h4 class="evo"><?php if (isset ($namePrev)) {
-            echo "prev evo: ", ucfirst($namePrev);
+            echo "Prev evo: ", ucfirst($namePrev);
         } else if (isset ($nameNextEvo)) {
             echo "It's a baby, next evo is: ", ucfirst($nameNextEvo);
         } ?></h4>
@@ -108,7 +108,8 @@ if (isset ($_GET["name"])) {
         } ?></h4>
     <img class="img_prev" src="<?php if (isset ($imgPrev)) {
         echo $imgPrev;
-    } else {
+    } else if (isset ($imgForNext)) {
+        echo $imgForNext;
     } ?>">
 </ol>
 </body>
